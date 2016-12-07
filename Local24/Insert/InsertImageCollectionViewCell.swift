7cb58eadfbb7aab07fc1e4a796c0200b42eac1e9
Locla24 <Local24@admins-mbp-6.as24.local>
2016-12-07 15:25:44 +0100

    celldelegates

--- Local24/Insert/InsertImageCollectionViewCell.swift
+++ Local24/Insert/InsertImageCollectionViewCell.swift
@@ -11,9 +11,28 @@ import UIKit
 class InsertImageCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var deleteImageButton: UIButton! {didSet {deleteImageButton.imageEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)}}
+    
+    var delegate: InsertImageCellDelegate?
+    
+    @IBAction func deleteImageButtonTapped(_ sender: UIButton) {
+    self.delegate?.buttonTapped(cell: self)
+    }
+    
+
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = 5
     }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+        self.delegate = nil
+    }
+}
+
+
+protocol InsertImageCellDelegate: class {
+    func buttonTapped(cell: InsertImageCollectionViewCell)
 }
