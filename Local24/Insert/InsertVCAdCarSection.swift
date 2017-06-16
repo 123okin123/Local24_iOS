566adf860e704ea104864b18aa2012ce8aeaf8db
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-06-16 09:39:55 +0200

    component update

--- Local24/Insert/InsertVCAdCarSection.swift
+++ Local24/Insert/InsertVCAdCarSection.swift
@@ -9,97 +9,96 @@
 import UIKit
 import Eureka
 
-
 extension InsertViewController {
+    
 
-func sectionForAdCar() -> Section {
-    let section = Section("adCarSection") {
-        $0.header?.title = nil
-        $0.hidden = Condition.function(["catTag"], {form in
-            if form.rowBy(tag: "catTag")?.value == "Auto" {
-                self.listing.component = AdCarComponent()
-                return false
-            } else {
-                return true
-            }
-          
-        })
-    }
     
-    section <<< PickerInputRow<Int>() {
-        $0.title = "Kilometerstand"
-        $0.value = 500
-        $0.options = arrayFrom(1, to: 500000, stepValue: 500)
-        }.onChange {
-            guard let component = self.listing.component as? AdCarComponent else {return}
-             component.mileAge = $0.value
-    }
     
-    section <<< PushRow<String>("makeTag") { row in
-        row.title = "Marke"
-        row.selectorTitle = row.title
-        row.options =  ["Alle Marken"]
-        NetworkManager.shared.getValuesForField("Make", entityType: .AdCar, completion: { values, error in
-            if error == nil {
-                row.options = values!
-            }
-        })
-        
-        }.onChange {
-            guard let value = $0.value else {return}
-            guard value != "Alle Marken" else {return}
-            
-            
-        }.onPresent { from, to in
-            self.applyCustomStyleOnSelectorVC(to)
-            to.enableDeselection = false
-            to.sectionKeyForValue = { option in
-                switch option {
-                case "Alle Marken": return ""
-                default: return " "
+    func sectionForAdCar() -> Section {
+        let section = Section("adCarSection") {
+            $0.header?.title = "Weitere Infos"
+            $0.hidden = Condition.function(["catTag"], {form in
+                if form.rowBy(tag: "catTag")?.value == "Auto" {
+                    if ((self.listing.component as? AdCarComponent) == nil) {
+                    self.listing.component = AdCarComponent()
+                    }
+                    return false
+                } else {
+                    return true
                 }
-            }
-    }
-    
-    section <<< PushRow<String>("modelTag") {
-        $0.hidden = Condition.function(["makeTag"], {form in
-            form.rowBy(tag: "modelTag")?.updateCell()
-            return (form.rowBy(tag: "makeTag")?.value == "Alle Marken")
-        })
-        $0.title = "Modell"
-        $0.selectorTitle = $0.title
-
-        $0.options = ["Alle Modelle"]
-        }.cellUpdate {cell, row in
-            
-        }.onChange {
-            guard let value = $0.value else {return}
-            guard value != "Alle Modelle" else {return}
-
-            //
-        }.onPresent { from, to in
-            self.applyCustomStyleOnSelectorVC(to)
-            (to.row as! PushRow).options.removeAll()
-            guard let makeRowValue = (self.form.rowBy(tag: "makeTag") as? PushRow<String>)?.value else {return}
-            NetworkManager.shared.getValuesForDepending(field: "Model", independendField: "Make", value: makeRowValue, entityType: .AdCar, completion: {values, error in
-                if var options = values {
-                    options.remove(at: 0)
-                    if (to.row as! PushRow).options != options {
+                
+            })
+        }
+        
+        section <<< PickerInputRow<Int>() {
+            $0.title = "Kilometerstand"
+            $0.value = 500
+            $0.options = arrayFrom(1, to: 500000, stepValue: 500)
+            }.onChange {
+                guard let component = self.listing.component as? AdCarComponent else {return}
+                component.mileAge = $0.value
+        }
+        
+        section <<< PushRow<String>("makeTag") { row in
+            row.title = "Marke"
+            row.selectorTitle = row.title
+            }.onChange {
+                guard let value = $0.value else {return}
+                guard value != "Alle Marken" else {return}
+                guard let component = self.listing.component as? AdCarComponent else {return}
+                component.make = $0.value
+            }.onPresent { from, to in
+                self.applyCustomStyleOnSelectorVC(to)
+                (to.row as! PushRow).options.removeAll()
+                NetworkManager.shared.getValuesForField("Make", entityType: .AdCar, completion: { values, error in
+                    if error == nil {
+                        (to.row as! PushRow).options = values!
+                        to.setupForm()
+                    }
+                })
+                to.enableDeselection = false
+                to.sectionKeyForValue = { option in
+                    switch option {
+                    case "Alle Marken": return ""
+                    default: return " "
+                    }
+                }
+        }
+        
+        section <<< PushRow<String>("modelTag") {
+            $0.hidden = Condition.function(["makeTag"], {form in
+                form.rowBy(tag: "modelTag")?.updateCell()
+                return (form.rowBy(tag: "makeTag")?.value == "Alle Marken")
+            })
+            $0.title = "Modell"
+            $0.selectorTitle = $0.title
+            }.cellUpdate {cell, row in
+                
+            }.onChange {
+                guard let value = $0.value else {return}
+                guard value != "Alle Modelle" else {return}
+                guard let component = self.listing.component as? AdCarComponent else {return}
+                component.model = $0.value
+            }.onPresent { from, to in
+                self.applyCustomStyleOnSelectorVC(to)
+                (to.row as! PushRow).options.removeAll()
+                guard let makeRowValue = (self.form.rowBy(tag: "makeTag") as? PushRow<String>)?.value else {return}
+                NetworkManager.shared.getValuesForDepending(field: "Model", independendField: "Make", value: makeRowValue, entityType: .AdCar, completion: {values, error in
+                    if var options = values {
+                        options.remove(at: 0)
                         (to.row as! PushRow).options = options
                         to.setupForm()
                     }
-                    
+                })
+                to.enableDeselection = false
+                to.sectionKeyForValue = { option in
+                    switch option {
+                    case "Alle Modelle": return ""
+                    default: return " "
+                    }
                 }
-            })
-            to.enableDeselection = false
-            to.sectionKeyForValue = { option in
-                switch option {
-                case "Alle Modelle": return ""
-                default: return " "
-                }
-            }
+        }
+        
+        return section
     }
-
-    return section
-}
 }
--- Local24/Model/AdCarComponent.swift
+++ Local24/Model/AdCarComponent.swift
@@ -14,6 +14,8 @@ import SwiftyJSON
 class AdCarComponent: AdComponent {
 
     var mileAge: Int?
+    var make: String?
+    var model: String?
     
     required init() {}
     
@@ -21,6 +23,8 @@ class AdCarComponent: AdComponent {
         let json = JSON(value)
         guard json != JSON.null else {return}
         self.mileAge = json["MileAge"].int
+        self.make = json["Make"].string
+        self.model = json["Model"].string
     }
     required init(searchIndexValue value: [AnyHashable : Any]) {
         let json = JSON(value)
@@ -28,18 +32,24 @@ class AdCarComponent: AdComponent {
         if let mileAgeString = json["mileage"].string {
             self.mileAge = Int(mileAgeString)
         }
+        self.make = json["makeName"].string
+        self.model = json["modelName"].string
+        
     }
-    
     
     func componentToJSON() -> [AnyHashable: Any]? {
         var values = [AnyHashable: Any]()
         values["mileAge"] = mileAge
+        values["make"] = make
+        values["model"] = model
         return values
     }
     
     func componentToRepresentableTupleArray() -> [(name: String?,value: String?)] {
         var tupleArray = [(name: String?, value: String?)]()
         if mileAge != nil { tupleArray.append((name: "Laufleistung",value: "\(mileAge!) km")) }
+        if make != nil { tupleArray.append((name: "Marke",value: "\(make!)")) }
+        if model != nil { tupleArray.append((name: "Modell",value: "\(model!)")) }
         return tupleArray
     }
 }
